f6e77dc6b1e7155c4c5add54b2ee3f91e7693745
darcyglover <darcyglover@users.noreply.github.com>
2020-07-01 11:21:33 +1000

    Summary

---
+++ pull-to-open/Assets/Scripts/Test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Test : MonoBehaviour
+{
+    public UnityEvent test;
+    GameObject wire, split1, split2;
+    void Start()
+    {
+        wire.SetActive(true);
+        split1.SetActive(false);
+        split2.SetActive(false);
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            test.Invoke();
+        }
+    }
+}
