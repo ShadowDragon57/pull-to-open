d9f7e596d836f8c8ed87ce4863253cdf0382becb
darcyglover <darcyglover@users.noreply.github.com>
2020-07-08 15:14:55 +1000

    ClickableObjects test

---
+++ pull-to-open/Assets/Scripts/ClickOn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickOn : MonoBehaviour
+{
+    [SerializeField]
+    private Material red;
+    [SerializeField]
+    private Material green;
+
+    private MeshRenderer myRend;
+
+    void Start()
+    {
+        myRend = GetComponent<MeshRenderer>();
+        
+    }
+
+    public void ClickMe() 
+    {
+        myRend.material = green;
+    }
+}
