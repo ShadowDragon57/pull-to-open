4d6e9fea29b3d1d045d63a3b537b8f7083598ad6
darcyglover <darcyglover@users.noreply.github.com>
2020-07-15 15:01:16 +1000

    light goes green if there are 7 wires

---
+++ pull-to-open/Assets/Scripts/Wires puzzle/Version 2 - light goes green if there are 7 wires!/Wires_ClickOn_V2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wires_ClickOn_V2 : MonoBehaviour
+{
+    [SerializeField]
+    private Material red;
+    [SerializeField]
+    private Material green;
+
+    private MeshRenderer wireRend;
+
+    public GameObject greenLight;
+    public GameObject redLight;
+
+    void Start()
+    {
+        redLight.SetActive(true);
+        wireRend = GetComponent<MeshRenderer>();
+    }
+
+    public void ClickMe()
+    {
+        wireRend.material = green;
+    }
+
+    public void WinClick()
+    {
+        wireRend.material = green;
+        redLight.SetActive(false);
+    }
+}
---
+++ pull-to-open/Assets/Scripts/Wires puzzle/Version 2 - light goes green if there are 7 wires!/Wires_Click_V2.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wires_Click_V2 : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask clickablesLayer;
+
+    private bool wrongClicked;
+    private GameObject[] wires;
+    void Start()
+    {
+        wrongClicked = false;
+        wires = GameObject.FindGameObjectsWithTag("Wire");
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit rayHit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, clickablesLayer))
+            {
+              if (rayHit.collider.CompareTag("SolutionWire"))
+                {                    
+                    if(wrongClicked == false)
+                    {
+                        if(wires.Length == 7)
+                        {                            
+                            rayHit.collider.GetComponent<Wires_ClickOn_V2>().WinClick();
+
+                        }
+                        else
+                        {
+                            rayHit.collider.GetComponent<Wires_ClickOn_V2>().ClickMe();
+                        }
+                    }
+                    if(wrongClicked == true)
+                    {
+                        rayHit.collider.GetComponent<Wires_ClickOn_V2>().ClickMe();
+                    }
+                }
+              if(rayHit.collider.CompareTag("Wire"))
+                {
+                    wrongClicked = true;
+                    rayHit.collider.GetComponent<Wires_ClickOn_V2>().ClickMe();
+                }
+            }
+        }
+    }
+}
