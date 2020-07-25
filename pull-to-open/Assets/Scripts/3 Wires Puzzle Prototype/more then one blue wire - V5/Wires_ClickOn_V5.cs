be210d950caee1639ca5e3e464f25fa16273fc56
darcyglover <darcyglover@users.noreply.github.com>
2020-07-25 15:25:36 +1000

    Working wires puzzle (3 wires)

---
+++ pull-to-open/Assets/Scripts/3 Wires Puzzle Prototype/more then one blue wire - V5/Wires_ClickOn_V5.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Wires_ClickOn_V5 : MonoBehaviour
+{
+    [SerializeField] 
+    private Material gone;
+    [SerializeField]
+    private GameObject redLight;
+
+    private MeshRenderer myRend;
+
+    void Start()
+    {
+        myRend = GetComponent<MeshRenderer>();
+        redLight.SetActive(true);
+    }
+
+    public void WinClick()
+    {
+        GameObject[] blueWires = GameObject.FindGameObjectsWithTag("BlueWire");
+        myRend.material = gone;
+        if(blueWires.Length > 1)
+        {
+            redLight.SetActive(false);
+        }
+    }
+
+    public void ClickMe()
+    {
+        myRend.material = gone; 
+    }
+}
---
+++ pull-to-open/Assets/Scripts/3 Wires Puzzle Prototype/more then one blue wire - V5/Wires_Click_V5.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wires_Click_V5 : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask clickablesLayer; 
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            RaycastHit rayHit; 
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, clickablesLayer))
+            {
+                if (rayHit.collider.name == "SolutionWire")
+                {
+                    rayHit.collider.GetComponent<Wires_ClickOn_V5>().WinClick();
+                }
+                else
+                {
+                    rayHit.collider.GetComponent<Wires_ClickOn_V5>().ClickMe();
+                }
+            }
+        }
+    }
+}
