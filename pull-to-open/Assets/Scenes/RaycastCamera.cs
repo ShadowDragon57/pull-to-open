44d14886361a1e8fede47e77a7307f11bae39959
ShadowDragon57 <1018984@student.sae.edu.au>
2020-07-29 11:38:48 +1000

    Light is fucked

---
+++ pull-to-open/Assets/Scenes/RaycastCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections;
+using UnityEngine;
+
+public class RaycastCamera : MonoBehaviour
+{
+    public GameObject wiresLight;
+    
+    Ray ray;
+    RaycastHit hit;
+   
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Light wireLight = wiresSelectLight.GetComponent("Light");
+        
+        if (Physics.Raycast(ray, out hit))
+        {
+            wireLight.enabled = !wireLight.enabled;
+        }
+    }
+}
