d8092ebcef5a2233d6b07b8f71a0512bd5467156
ShadowDragon57 <1018984@student.sae.edu.au>
2020-08-02 01:38:22 +1000

    Navigation within the room

--- pull-to-open/Assets/Scenes/RaycastCamera.cs
+++ pull-to-open/Assets/Scenes/RaycastCamera.cs
@@ -1,28 +1,85 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RaycastCamera : MonoBehaviour
 {
-    //public GameObject wirebox;
+    //Navigation Buttons
+    public GameObject LLeft;
+    public GameObject LRight;
+    public GameObject LFront;
+    public GameObject LBack;
+    public GameObject RLeft;
+    public GameObject RRight;
+    public GameObject RFront;
+    public GameObject RBack;
+
+    //Puzzle Buttons
     public GameObject WireButton;
     public GameObject BookcaseButton;
     public GameObject CardsButton;
     
     Ray ray;
     RaycastHit hit;
-   
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        WireButton.SetActive(false);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+       
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        
+
+        //Room Navigation UI
+        if (Physics.Raycast(ray, out hit) & hit.collider.name == "L-Left Wall")
+        {
+            LLeft.SetActive(true);
+        }
+        else if (Physics.Raycast(ray, out hit) & hit.collider.name == "L-Right Wall")
+        {
+            LRight.SetActive(true);
+        }
+        else if (Physics.Raycast(ray, out hit) & hit.collider.name == "L-Front Wall")
+        {
+            LFront.SetActive(true);
+        }
+        else if (Physics.Raycast(ray, out hit) & hit.collider.name == "L-Back Wall")
+        {
+            LBack.SetActive(true);
+        }
+        else if (Physics.Raycast(ray, out hit) & hit.collider.name == "R-Left Wall")
+        {
+            RLeft.SetActive(true);
+        }
+        else if (Physics.Raycast(ray, out hit) & hit.collider.name == "R-Right Wall")
+        {
+            RRight.SetActive(true);
+        }
+        else if (Physics.Raycast(ray, out hit) & hit.collider.name == "R-Front Wall")
+        {
+            RFront.SetActive(true);
+        }
+        else if (Physics.Raycast(ray, out hit) & hit.collider.name == "R-Back Wall")
+        {
+            RBack.SetActive(true);
+        }
+        else
+        {
+            LLeft.SetActive(false);
+            LRight.SetActive(false);
+            LFront.SetActive(false);
+            LBack.SetActive(false);
+            RLeft.SetActive(false);
+            RRight.SetActive(false);
+            RFront.SetActive(false);
+            RBack.SetActive(false);
+        }
+
+        //Puzzle UI
         if (Physics.Raycast(ray, out hit) & hit.collider.name == "wirebox")
         { 
             WireButton.SetActive(true);
---
+++ pull-to-open/Assets/Scenes/RoomMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomMovement : MonoBehaviour
+{
+    /*
+     *  Back = -254.5
+     *  Left = -114.5
+     *  Front - 25.5
+     *  Right = 164.5
+     */
+
+    public GameObject RoomCamera;
+
+    public void NavLeft()
+    {
+        RoomCamera.transform.position = new Vector3(-114.5f, 95f, -160f);
+    }
+
+    public void NavRight()
+    {
+        RoomCamera.transform.position = new Vector3(164.5f, 95f, -160f);
+    }
+
+    public void NavFront()
+    {
+        RoomCamera.transform.position = new Vector3(25.5f, 95f, -160f);
+    }
+
+    public void NavBack()
+    {
+        RoomCamera.transform.position = new Vector3(-254.5f, 95f, -160f);
+    }
+}
